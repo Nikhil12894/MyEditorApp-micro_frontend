b8eeea9fee13f1c0cddce1cde2f8b216dd1f0ed6
Nalin Kumar <nalin12894+github@gmail.com>
2024-07-25 21:32:54 +0530

    EDITOR_COMPONENTS_URL const

--- host_app_cdk8s/consents.ts
+++ host_app_cdk8s/consents.ts
@@ -1,3 +1,5 @@
 export const IMAGE_NAME = "micro-frontend-host-app";
 export const IMAGE_VERSION = "v1.0.0";
 export const INGRESS_HOST = "micro-frontend-editor.learnwithnk.in";
+
+export const EDITOR_COMPONENTS_URL = "https://app-editor.learnwithnk.in/assets/remoteEntry.js";
