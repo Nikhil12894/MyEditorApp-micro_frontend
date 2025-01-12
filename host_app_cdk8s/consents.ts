dafb7e9d45a72a70f0b7ec72d0b66ac06793dc5f
Nalin Kumar <nalin12894+github@gmail.com>
2025-01-12 23:14:52 +0530

    updated the image version

--- host_app_cdk8s/consents.ts
+++ host_app_cdk8s/consents.ts
@@ -1,3 +1,3 @@
 export const IMAGE_NAME = "micro-frontend-host-app";
-export const IMAGE_VERSION = "v1.0.7";
+export const IMAGE_VERSION = "v1.0.8";
 export const INGRESS_HOST = "nkblog.explorewithnk.com";
